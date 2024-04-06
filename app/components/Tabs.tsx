776a6d33e174f4ea6361e1bb5b8e80888c40c25f
midCenturyDude7 <mgriffes@gmail.com>
2024-04-05 21:52:39 -0700

    Setting up initial file/component structure

---
+++ app/components/Tabs.tsx
@@ -0,0 +1,11 @@
+// Tabs.tsx Component
+"use client";
+import React from "react";
+
+export default function Tabs() {
+  return (
+    <div>
+      <h1>Tabs</h1>
+    </div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
-import Image from "next/image";
+import Tabs from "./components/Tabs";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+    <div className="flex min-h-screen flex-col items-center justify-center text-4xl">
+      <Tabs />
+    </div>
   );
 }
